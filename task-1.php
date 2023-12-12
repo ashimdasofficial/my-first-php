78a04cd6a9560f4c86e8954eb81310305a0b2b0c
ashimdasofficial <ashim2828@gmail.com>
2023-12-13 02:32:31 +0600

    upgrade code

--- task-1.php
+++ task-1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<head lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>This is php</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi assumenda et veritatis mollitia maxime labore accusamus in non excepturi, aliquam exercitationem optio? Iure totam ipsum quos minus sed id.</p>
 </body>
-</html>
+</head>
