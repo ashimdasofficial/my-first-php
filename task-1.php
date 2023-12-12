e2b21f4a47a61584a7995b0594c5e93fb74c173f
ashimdasofficial <ashim2828@gmail.com>
2023-12-13 02:44:14 +0600

    new update

--- task-1.php
+++ task-1.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<head lang="en">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h1>This is php</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi assumenda et veritatis mollitia maxime labore accusamus in non excepturi, aliquam exercitationem optio? Iure totam ipsum quos minus sed id.</p>
+    <h1>This is Ashim Das</h1>
 </body>
-</head>
+</html>
